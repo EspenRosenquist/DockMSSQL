522373ed1ac9f436595c893940efec7c8f9a77b5
Espen Rosenquist <espen.rosenquist@gmail.com>
2023-11-02 12:22:01 +0100

    adding python service

--- setup.sh
+++ setup.sh
@@ -3,9 +3,8 @@
 # Create the network
 docker network create --attachable --driver bridge sql_network
 
-# Build the custom SQL image (assuming you have a Dockerfile in the current directory)
-#docker build --build-arg SQL_SERVER_ROOT=$SQL_SERVER_ROOT -t dev_sqlserver:2019 .
-docker build -t dev_sqlserver:2019 .
+# Build the custom images
+docker build -t dev_sqlserver:2019 -f ./Dockerfiles/mssql-Dockerfile .
 
 # Give user mssql the rights to write to bound disk:
 sudo chown -R 10001 ./mssql
